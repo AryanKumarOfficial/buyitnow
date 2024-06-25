db47fdcaa39e8979bb23e28cad772eed1f2a9e7c
Aryan Kumar <Aryanak9163@gmail.com>
2024-06-25 12:03:31 +0530

    #restart

--- pages/api/products/[id].js
+++
@@ -1,9 +0,0 @@
-import nc from 'next-connect';
-import dbConnect from '../../../backend/config/dbConnect';
-import { getProduct } from '../../../backend/controllers/productControllers'
-dbConnect();
-
-const handler = nc()
-handler.get(getProduct)
-
-export default handler;
