eadd8045ac1ae73cc0c2f6436eb60bc0b642c69d
Aryan Kumar <Aryanak9163@gmail.com>
2024-06-29 02:27:15 +0530

    chore: Refactor productControllers.js for improved API functionality

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,5 @@
 import Product from "../Models/Product";
-
+import APIFilters from "../utils/APIFilters";
 
 export const newProduct = async (request) => {
     try {
@@ -13,28 +13,29 @@ export const newProduct = async (request) => {
             success: true,
         }, { status: 201 });
     } catch (error) {
-        return Response.json({ success: false, error: error.message }, { status: 500 })
+        return Response.json({ success: false, error: error.message }, { status: 500 });
     }
+};
 
-}
+export const getProducts = async (request) => {
+    const keyword = request.nextUrl.searchParams.get("keyword");
 
-export const getProducts = async (_request) => {
+    const apiFilters = new APIFilters(Product.find(), { keyword }).search();
+
     try {
-        const products = await Product.find()?.sort({ createdAt: -1 });
+        const products = await apiFilters.query;
         return Response.json({
             products: products,
             success: true,
         }, { status: 200 });
     } catch (error) {
-        return Response.json({ success: false, error: error.message }, { status: 500 })
+        return Response.json({ success: false, error: error.message }, { status: 500 });
     }
-}
+};
 
 export const getProductById = async (request, { params }) => {
-
     try {
         const { id } = params;
-        console.log(typeof (id), "id");
         const product = await Product.findById(id);
         if (!product) {
             return Response.json({ success: false, error: "Product not found" }, { status: 404 });
@@ -43,8 +44,7 @@ export const getProductById = async (request, { params }) => {
             product: product,
             success: true,
         }, { status: 200 });
+    } catch (error) {
+        return Response.json({ success: false, error: error.message }, { status: 500 });
     }
-    catch (error) {
-        return Response.json({ success: false, error: error.message }, { status: 500 })
-    }
-}
+};
